046e09bcbe05b13b8208ada28d455f28ae917da0
Mari_Mizuta <53279713+viaNEONIA@users.noreply.github.com>
2026-02-16 22:04:13 +0900

    1

---
+++ Your-medical-is-small/config.cpp
@@ -0,0 +1,31 @@
+class CfgPatches
+{
+	class HardcoreClothing_Override
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={
+			"DZ_Data"
+		};
+	};
+};
+
+class CfgMods
+{
+	class HardcoreClothing_Override
+	{
+		dir="HardcoreClothing_Override";
+		picture="";
+		action="";
+		hideName=1;
+		hidePicture=1;
+		name="HardcoreClothing_Override";
+		credits="SERRE";
+		author="SERRE";
+		authorID="";
+		version="0.3";
+		extra=0;
+		type="mod";
+	};
+};
